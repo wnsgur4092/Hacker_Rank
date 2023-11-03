a0ab14d66fddec5d9604ed45e5e61929304fb861
Jun <wnsgur4092@gmail.com>
2023-11-03 21:43:46 +1100

    #Easy-Breaking the Records

--- Implementation/Breaking the Records.swift
+++ Implementation/Breaking the Records.swift
@@ -1,25 +1,12 @@
 import Foundation
 
-/*
- * Complete the 'breakingRecords' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts INTEGER_ARRAY scores as parameter.
- */
-
-// func breakingRecords(scores: [Int]) -> [Int] {
-//     // Write your code here
-//     var newArray = Set(scores)   
-//     print(new)
-// }
-
 func breakingRecords(scores: [Int]) -> [Int] {
     // Initialize the record variables.
     var high = scores.first!
     var low = scores.first!
 
-    print(high)
-    print(low)
+    // print(high)
+    // print(low)
 
     var max = 0
     var min = 0
@@ -27,20 +14,18 @@ func breakingRecords(scores: [Int]) -> [Int] {
     for score in scores {
         if score > high {
             high = score
-            print("highest:\(high)" )
+            // print("highest:\(high)" )
             max += 1
         } else if score < low {
             low = score
-            print("lowest:\(low)" )
+            // print("lowest:\(low)" )
             min += 1
         }
     }
-
-    // Return the counts in an array.
     return [max, min]
 }
 
 let scores = [10, 5, 20, 20, 4, 5, 2, 25, 1]
 let recordBreaks = breakingRecords(scores: scores)
-print(recordBreaks) // Output should be [2, 4] for the example given.
+print(recordBreaks)
 
