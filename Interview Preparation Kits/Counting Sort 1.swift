463e3022c4edd6d44a2e8bb498eab5019d21d5fa
Jun <wnsgur4092@gmail.com>
2024-01-02 18:53:46 +1100

    [Week1]Day 2 - Counting Sort 1

---
+++ Interview Preparation Kits/Counting Sort 1.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  CodeTest
+//
+//  Created by JunHyuk Lim on 2/1/2024.
+//
+
+import Foundation
+
+func countingSort(arr: [Int]) -> [Int] {
+    // Write your code here
+    var result = Array(repeating: 0, count: 100)
+    for element in arr {
+        result[element] = result[element] + 1
+    }
+    
+    return result
+}
+
+
+// Test the function
+let inputArray = [63, 54, 17, 78, 43, 70, 32, 97, 16, 94, 74, 18, 60, 61, 35, 83, 13, 56, 75, 52, 70, 12, 24, 37, 17, 0, 16, 64, 34, 81, 82, 24, 69, 2, 30, 61, 83, 37, 97, 16, 70, 53, 0, 61, 12, 17, 97, 67, 33, 30, 49, 70, 11, 40, 67, 94, 84, 60, 35, 58, 19, 81, 16, 14, 68, 46, 42, 81, 75, 87, 13, 84, 33, 34, 14, 96, 7, 59, 17, 98, 79, 47, 71, 75, 8, 27, 73, 66, 64, 12, 29, 35, 80, 78, 80, 6, 5, 24, 49, 82]
+let result = countingSort(arr: inputArray)
+print(result)
